4d2f7541e228ea74fd51bce96198e2df05f8dd02
MatyBene <benedettimaty@gmail.com>
2024-08-15 14:49:19 -0300

    crea class Empleado con sus caracteristicas, el constructor y getters y setters

---
+++ guias/guia1/src/Empleado.java
@@ -0,0 +1,46 @@
+public class Empleado {
+
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private double salario;
+
+    public Empleado(String dni, String nombre, String apellido, double salario) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salario = salario;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+}
