9896d4be162b1d1544aab3516fe49866a7343292
MatyBene <benedettimaty@gmail.com>
2024-08-18 22:20:10 -0300

    agrega ejercicio 10 guia adicional 2

--- guias-adicionales/guiaAdicional2/src/App.java
+++ guias-adicionales/guiaAdicional2/src/App.java
@@ -163,6 +163,28 @@ public class App {
 //                    10.2Kg.? ¿Cuántas con menos de 9.8 Kg.?
 //                    b) La cantidad total de piezas procesadas.
                 {
+                    double peso;
+                    int i = 0, j = 0, k = 0;
+
+                    do {
+                        System.out.println("Ingrese el peso de la pieza: ");
+                        peso = input.nextDouble();
+                        input.nextLine();
+                        if(peso >= 9.8 && peso <= 10.2){
+                            i++;
+                        }
+                        if(peso < 9.8 && peso != 0){
+                            k++;
+                        }
+                        if(peso > 10.2){
+                            j++;
+                        }
+                    } while(peso != 0);
+
+                    System.out.println("Piezas entre 9.8 kg y 10.2 kg: " + i);
+                    System.out.println("Piezas mayores a 10.2 kg: " + j);
+                    System.out.println("Piezas menores a 9.8 kg: " + k);
+                    System.out.println("Piezas totales: " + (i + j + k));
 
                     break;
                 }
