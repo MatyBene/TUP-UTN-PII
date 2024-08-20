75711c5005c7e241e509ed1d89d7adbb67c8628c
MatyBene <benedettimaty@gmail.com>
2024-08-20 14:39:14 -0300

    agrega funciones para calculadora

--- guias-adicionales/guiaAdicional3/src/App.java
+++ guias-adicionales/guiaAdicional3/src/App.java
@@ -30,6 +30,22 @@ public class App {
         return resultado;
     }
 
+    public static double suma(double a, double b){
+        return (a + b);
+    }
+
+    public static double resta(double a, double b){
+        return (a - b);
+    }
+
+    public static double multiplicacion(double a, double b){
+        return (a * b);
+    }
+
+    public static double division(double a, double b){
+        return (a / b);
+    }
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
