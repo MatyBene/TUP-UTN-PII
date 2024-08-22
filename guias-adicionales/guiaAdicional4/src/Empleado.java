8cf70dbc9f3aaa5a55a6f640ba7c850d1aeb8b54
MatyBene <benedettimaty@gmail.com>
2024-08-22 16:15:08 -0300

    resuelve ejercicio 7 guia adicional 4

--- guias-adicionales/guiaAdicional4/src/Empleado.java
+++ guias-adicionales/guiaAdicional4/src/Empleado.java
@@ -17,7 +17,63 @@ public class Empleado {
     private String apellido;
     private double salario;
 
+    public Empleado() {
+    }
 
+    public Empleado(String dni, String nombre, String apellido, double salario) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.salario = salario;
+    }
 
+    public double salarioAnual(){
+        return (this.salario * 12);
+    }
 
+    public void aumentarSueldo(double porcentaje){
+        this.salario *= (1 + (porcentaje / 100));
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "dni='" + dni + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
