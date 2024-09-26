8e25c1802b65779a9169241959d426faf2a3e3dd
Matias Benedetti <135931972+MatyBene@users.noreply.github.com>
2024-09-26 20:04:59 -0300

    castea los resultados de la calculadora de enteros

--- guia6/e3/E3.java
+++ guia6/e3/E3.java
@@ -7,12 +7,12 @@ public class E3 {
 
         System.out.println("-- Calculadora de enteros --");
         OperacionMatematica<Integer> calculadoraEnteros = new OperacionMatematica<>();
-        calculadoraEnteros.setNumeros(10, 2);
-        System.out.println("Suma: " + calculadoraEnteros.sumar());
-        System.out.println("Resta: " + calculadoraEnteros.restar());
-        System.out.println("Multiplicacion: " + calculadoraEnteros.multiplicar());
+        calculadoraEnteros.setNumeros(10, 0);
+        System.out.println("Suma: " + (int) calculadoraEnteros.sumar());
+        System.out.println("Resta: " + (int) calculadoraEnteros.restar());
+        System.out.println("Multiplicacion: " + (int) calculadoraEnteros.multiplicar());
         try{
-            System.out.println("Division: " + calculadoraEnteros.dividir());
+            System.out.println("Division: " + (int) calculadoraEnteros.dividir());
         } catch(ArithmeticException e){
             System.out.println(e.getMessage());
         }
@@ -20,7 +20,7 @@ public class E3 {
 
         System.out.println("-- Calculadora de double --");
         OperacionMatematica<Double> calculadoraDoubles = new OperacionMatematica<>();
-        calculadoraDoubles.setNumeros(10.4, 2.1);
+        calculadoraDoubles.setNumeros(10.4, 0.0);
         System.out.println("Suma: " + calculadoraDoubles.sumar());
         System.out.println("Resta: " + calculadoraDoubles.restar());
         System.out.println("Multiplicacion: " + calculadoraDoubles.multiplicar());
