6f3dd22a2fc5f8f83e51cc4707744458fde66480
Matias Benedetti <135931972+MatyBene@users.noreply.github.com>
2024-10-16 19:51:50 -0300

    agrega metodos grabar y leer en JsonService

--- guia7/src/service/JsonService.java
+++ guia7/src/service/JsonService.java
@@ -11,5 +11,27 @@ import java.nio.file.Paths;
 
 public class JsonService {
 
+    public static void grabar(JSONArray array){
+        try{
+            FileWriter file = new FileWriter("archivo.json");
+            file.write(arrray.toString());
+            file.flush();
+            file.close();
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONTokener leer(String archivo){
+        JSONTokener tokener = null;
+
+        try{
+            tokener = new JSONTokener(new FileReader(archivo);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+
+        return tokener;
+    }
 
 }
